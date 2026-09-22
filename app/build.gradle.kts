eb2b18dfd7438c00b2319c8e085f9f530f850cca
MD SIDIK <149406522+sidik11@users.noreply.github.com>
2026-09-22 23:10:50 +0530

    release: prepare MS Gallery 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0.0"
     }
     buildTypes {
         release {
