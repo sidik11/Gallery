3122dfccf7163f5c5f5356861d446b968e5798b0
MD SIDIK <149406522+sidik11@users.noreply.github.com>
2026-09-22 22:22:45 +0530

    fix: import vault dialog

--- app/src/main/java/com/sidik/msgallery/ui/VaultScreen.kt
+++ app/src/main/java/com/sidik/msgallery/ui/VaultScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
 import androidx.media3.common.MediaItem as PlayerMediaItem
 import androidx.media3.datasource.ByteArrayDataSource
 import androidx.media3.datasource.DataSource
