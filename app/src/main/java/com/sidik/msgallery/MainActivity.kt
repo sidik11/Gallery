fdd4eba51cabb25296e384c2647717047752348d
MD SIDIK <149406522+sidik11@users.noreply.github.com>
2026-09-22 21:09:01 +0530

    fix: import Compose viewer dialog

--- app/src/main/java/com/sidik/msgallery/MainActivity.kt
+++ app/src/main/java/com/sidik/msgallery/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
