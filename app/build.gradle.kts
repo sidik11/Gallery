c861009351618e77d206bbc178cc27aa1b782c41
MD SIDIK <149406522+sidik11@users.noreply.github.com>
2026-09-22 21:56:50 +0530

    test: add Mockito for Android Uri unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,5 +53,6 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.7.1")
     implementation("androidx.media3:media3-ui:1.7.1")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.18.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
